950036737fc868404a0cc4b2c227e51a9928f289
https://github.com/suhibalobeidat <sohaibalobeidat@gmail.com>
2022-11-20 22:53:41 +0300

    rlib work in progress

---
+++ call_backs.py
@@ -0,0 +1,9 @@
+from ray.rllib.algorithms.callbacks import DefaultCallbacks
+from ray.rllib.policy.sample_batch import SampleBatch
+
+class MyCallBacks(DefaultCallbacks):
+    def __init__(self):
+        super.__init__()
+
+    def on_sample_end(self, *, worker: "RolloutWorker", samples: SampleBatch, **kwargs) -> None:
+        return super().on_sample_end(worker=worker, samples, **kwargs)
